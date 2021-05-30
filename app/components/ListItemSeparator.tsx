5d5cb2a38277eae70ad95588170c313e8a121e05
Nacen <overlordnacen@gmail.com>
2021-05-30 11:16:36 +0800

    Fix background color not showing up

--- app/components/ListItemSeparator.tsx
+++ app/components/ListItemSeparator.tsx
@@ -3,19 +3,18 @@ import { StyleSheet, View } from "react-native";
 import { colors } from "../config/colors";
 
 interface Props {
-  backgroundColor: string;
+  backgroundColor?: string;
 }
 
-export const ListItemSeparator = ({ backgroundColor }: Props) => {
-  return (
-    <View style={[styles.separator, { backgroundColor: backgroundColor }]} />
-  );
-};
+export const ListItemSeparator = ({ backgroundColor }: Props) => (
+  <View
+    style={[
+      styles.separator,
+      { backgroundColor: backgroundColor ? backgroundColor : colors.black },
+    ]}
+  />
+);
 
 const styles = StyleSheet.create({
-  separator: {
-    width: "100%",
-    height: 1,
-    backgroundColor: colors.gray,
-  },
+  separator: { width: "100%", height: 1, backgroundColor: colors.black },
 });
