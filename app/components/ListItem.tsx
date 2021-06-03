89d08cd04f6cc31c783140558c00e1c6664c0870
Nacen <overlordnacen@gmail.com>
2021-06-03 08:53:02 +0800

    Update ListItem and Add ListItemDeleteAction

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -6,29 +6,57 @@ import {
   Image,
   ImageSourcePropType,
   ViewStyle,
+  TouchableHighlight,
+  ImageStyle,
 } from "react-native";
+import Swipeable from "react-native-gesture-handler/Swipeable";
 
 import { colors } from "../config/colors";
 
 interface Props {
-  image: ImageSourcePropType | string;
+  image?: ImageSourcePropType | string;
   title: string;
-  subTitle: string;
+  subTitle?: string;
   listItemStyle?: ViewStyle;
+  onPress?: () => void;
+  renderLeftActions?: () => React.ReactNode;
+  renderRightActions?: () => React.ReactNode;
+  imageStyle?: ImageStyle;
+  icon?: () => React.ReactNode;
 }
 
-export const ListItem = ({ image, title, subTitle, listItemStyle }: Props) => {
+export const ListItem = ({
+  image,
+  title,
+  subTitle,
+  listItemStyle,
+  onPress,
+  renderLeftActions,
+  renderRightActions,
+  imageStyle,
+  icon,
+}: Props) => {
   return (
-    <View style={[styles.container, listItemStyle]}>
-      <Image
-        source={typeof image === "string" ? { uri: image } : image}
-        style={styles.image}
-      />
-      <View style={styles.userDetails}>
-        <Text style={styles.title}>{title}</Text>
-        <Text style={styles.subTitle}>{subTitle}</Text>
-      </View>
-    </View>
+    <Swipeable
+      renderRightActions={renderRightActions}
+      renderLeftActions={renderLeftActions}
+    >
+      <TouchableHighlight underlayColor={colors.gray} onPress={onPress}>
+        <View style={[styles.container, listItemStyle]}>
+          {image && (
+            <Image
+              source={typeof image === "string" ? { uri: image } : image}
+              style={[styles.image, imageStyle]}
+            />
+          )}
+          {icon && icon()}
+          <View style={styles.userDetails}>
+            <Text style={styles.title}>{title}</Text>
+            <Text style={styles.subTitle}>{subTitle}</Text>
+          </View>
+        </View>
+      </TouchableHighlight>
+    </Swipeable>
   );
 };
 
@@ -41,7 +69,7 @@ const styles = StyleSheet.create({
     width: 75,
     height: 75,
     borderRadius: 50,
-    marginRight: 20,
+    marginRight: 10,
   },
   userDetails: {
     paddingVertical: 2.5,
