c09a24bd6260dd5e2f6586c6dcfadb93905dedca
Nacen <overlordnacen@gmail.com>
2021-06-06 13:56:03 +0800

    Add tempdata to config folder

---
+++ app/config/tempData.js
@@ -0,0 +1,20 @@
+export const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Games", value: 3 },
+];
+
+export const LISTINGS_DATA = [
+  {
+    id: "1",
+    image: require("../assets/jacket.jpg"),
+    title: "Red jacket for sale",
+    price: "$100",
+  },
+  {
+    id: "2",
+    image: require("../assets/couch.jpg"),
+    title: "Couch in great condition",
+    price: "$1000",
+  },
+];
