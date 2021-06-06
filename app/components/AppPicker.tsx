50f6d9ea0fd87bc6a196508ff623864b6239ccae
Nacen <overlordnacen@gmail.com>
2021-06-06 13:58:40 +0800

    Add a custom picker

---
+++ app/components/AppPicker.tsx
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import {
+  Button,
+  FlatList,
+  Modal,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  ViewStyle,
+} from "react-native";
+import { globalStyles } from "../config/styles";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { materialType } from "../config/materialTypes";
+import { AppText } from "./AppText";
+import Screen from "./Screen";
+import PickerItem from "./PickerItem";
+
+export type Category = {
+  label: string;
+  value: number;
+};
+
+interface Props {
+  icon?: materialType["name"];
+  placeholder: string;
+  items: Category[];
+  onSelectItem: (item: Category) => void;
+  selectedItem?: Category;
+}
+
+export const AppPicker = ({
+  icon,
+  placeholder,
+  items,
+  selectedItem,
+  onSelectItem,
+}: Props) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const openModal = () => setIsModalVisible(true);
+  const closeModal = () => setIsModalVisible(false);
+
+  return (
+    <>
+      <TouchableWithoutFeedback onPress={openModal}>
+        <View style={styles.container}>
+          {icon && (
+            <MaterialCommunityIcons
+              name={icon}
+              style={styles.icon}
+              size={20}
+              color={globalStyles.colors.darkGray}
+            />
+          )}
+          <AppText style={styles.text}>
+            {selectedItem ? selectedItem.label : placeholder}
+          </AppText>
+          <MaterialCommunityIcons
+            name="chevron-down"
+            size={20}
+            color={globalStyles.colors.darkGray}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+
+      <Modal visible={isModalVisible} animationType="slide">
+        {/* <Screen> */}
+        <FlatList
+          data={items}
+          keyExtractor={(item) => item.value.toString()}
+          renderItem={({ item }) => (
+            <PickerItem
+              label={item.label}
+              onPress={() => {
+                closeModal();
+                onSelectItem(item);
+              }}
+            />
+          )}
+        />
+        <Button onPress={closeModal} title="Close" />
+
+        {/* </Screen> */}
+      </Modal>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: globalStyles.colors.lightGray,
+    borderRadius: 25,
+    flexDirection: "row",
+    width: "100%",
+    padding: 15,
+    marginVertical: 10,
+    alignItems: "center",
+  },
+  icon: {
+    marginRight: 10,
+  },
+  text: {
+    flex: 1,
+  },
+});
