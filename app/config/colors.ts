c32cc94ed840256e1b5e1a54e140a420fbac7bf0
Nacen <overlordnacen@gmail.com>
2021-06-06 13:55:45 +0800

    Update file from .js to .ts

--- app/config/colors.ts
+++ app/config/colors.ts
@@ -5,7 +5,7 @@ export const colors = {
   white: "#fff",
   green: "#52b788",
   gray: "#dddddd",
-  darkGray: "#495057",
+  darkGray: "#0c0c0c",
   lightGray: "#f9f3f3",
   danger: "#ff5252",
   yellow: "#ffe66d",
