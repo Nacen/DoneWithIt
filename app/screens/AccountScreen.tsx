9054144de8e24be2a22b1bd96e9a6c9b2c43cab0
Nacen <overlordnacen@gmail.com>
2021-06-06 10:37:21 +0800

    Update and Add dummy data to AccountScreen Component

--- app/screens/AccountScreen.tsx
+++ app/screens/AccountScreen.tsx
@@ -1,45 +1,79 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
 import { ListItem } from "../components/ListItem";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import Screen from "../components/Screen";
 import { colors } from "../config/colors";
 import { Icon } from "../components/Icon";
+import { materialType } from "../config/materialTypes";
 
 interface Props {}
+
+type MenuItem = {
+  id: string;
+  title: string;
+  icon: {
+    name: materialType["name"];
+    backgroundColor: string;
+  };
+};
+
+const MenuItems: MenuItem[] = [
+  {
+    id: "1",
+    title: "My Listings",
+    icon: {
+      name: "format-list-bulleted",
+      backgroundColor: colors.danger,
+    },
+  },
+  {
+    id: "2",
+    title: "My Messages",
+    icon: {
+      name: "email",
+      backgroundColor: colors.secondary,
+    },
+  },
+];
 
 export const AccountScreen = (props: Props) => {
   return (
     <Screen style={styles.accountScreen}>
       <ListItem
-        image={require("../assets/mosh.jpg")}
+        ImageComponent={
+          <Image
+            source={require("../assets/mosh.jpg")}
+            style={styles.accountImage}
+          />
+        }
         title="Mosh Hamedani"
         subTitle="programmingwithmosh@gmail.com"
         listItemStyle={styles.listingMargin}
       />
+      <View style={styles.listingMargin}>
+        <FlatList
+          data={MenuItems}
+          renderItem={({ item }) => (
+            <ListItem
+              ImageComponent={
+                <Icon
+                  name={item.icon.name}
+                  size={50}
+                  backgroundColor={item.icon.backgroundColor}
+                />
+              }
+              title={item.title}
+            />
+          )}
+          keyExtractor={(menuItem) => menuItem.title}
+        />
+      </View>
       <ListItem
-        icon={() => (
-          <Icon
-            name="format-list-bulleted"
-            size={50}
-            backgroundColor={colors.danger}
-          />
-        )}
-        title="My Listings"
-        listItemStyle={styles.listingMargin}
-      />
-      <ListItem
-        icon={() => (
-          <Icon name="email" size={50} backgroundColor={colors.secondary} />
-        )}
-        title="My Messages"
-        listItemStyle={styles.listingMargin}
-      />
-      <ListItem
-        icon={() => (
+        ImageComponent={
           <Icon name="logout" size={50} backgroundColor={colors.yellow} />
-        )}
+        }
         title="Log Out"
       />
     </Screen>
@@ -66,5 +100,10 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     alignSelf: "center",
     padding: 10,
+  },
+  accountImage: {
+    width: 60,
+    height: 60,
+    borderRadius: 50,
   },
 });
