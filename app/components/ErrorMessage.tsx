2fb37bc712f998c6a5ebc836c21d93c0c276f808
Nacen <overlordnacen@gmail.com>
2021-06-08 12:50:00 +0800

    Create ErrorMessage Component

---
+++ app/components/ErrorMessage.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, TextStyle } from "react-native";
+import { AppText } from "./AppText";
+
+interface Props {
+  error: string | undefined;
+  visible: boolean | undefined;
+  style?: TextStyle;
+}
+
+export const ErrorMessage = ({ error, visible, style }: Props) => {
+  console.log(error);
+  if (!visible || !error) return null;
+  return (
+    <AppText style={StyleSheet.flatten([styles.error, style])}>{error}</AppText>
+  );
+};
+
+const styles = StyleSheet.create({
+  error: {
+    color: "red",
+    padding: 10,
+  },
+});
