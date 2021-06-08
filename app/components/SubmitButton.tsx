3a77c4f91014b0ed4ede8e01192c6c1d038f0e61
Nacen <overlordnacen@gmail.com>
2021-06-08 12:49:35 +0800

    Create SubmitButton Component

---
+++ app/components/SubmitButton.tsx
@@ -0,0 +1,14 @@
+import { useFormikContext } from "formik";
+import React from "react";
+
+import { AppButton } from "./AppButton";
+
+interface Props {
+  label: string;
+  [key: string]: any;
+}
+
+export const SubmitButton = ({ label, ...otherProps }: Props) => {
+  const { handleSubmit } = useFormikContext();
+  return <AppButton label={label} onPress={handleSubmit} {...otherProps} />;
+};
