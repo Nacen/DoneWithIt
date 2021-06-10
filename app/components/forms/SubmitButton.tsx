9b84fa11b0873d7d437040b284f46d8eb50a496f
Nacen <overlordnacen@gmail.com>
2021-06-10 13:12:05 +0800

    Add props for styling SubmitButton component

--- app/components/forms/SubmitButton.tsx
+++ app/components/forms/SubmitButton.tsx
@@ -1,14 +1,30 @@
 import { useFormikContext } from "formik";
 import React from "react";
+import { TextStyle, ViewStyle } from "react-native";
 
 import { AppButton } from "../AppButton";
 
 interface Props {
   label: string;
+  labelStyle?: TextStyle;
+  buttonStyle?: ViewStyle;
   [key: string]: any;
 }
 
-export const SubmitButton = ({ label, ...otherProps }: Props) => {
+export const SubmitButton = ({
+  label,
+  labelStyle,
+  buttonStyle,
+  ...otherProps
+}: Props) => {
   const { handleSubmit } = useFormikContext();
-  return <AppButton label={label} onPress={handleSubmit} {...otherProps} />;
+  return (
+    <AppButton
+      label={label}
+      onPress={handleSubmit}
+      buttonStyle={buttonStyle}
+      labelStyle={labelStyle}
+      {...otherProps}
+    />
+  );
 };
