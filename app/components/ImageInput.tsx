30817cf452f7f85c686ed2bba8196d81b6445837
NacenVi <overlordnacen@gmail.com>
2021-06-26 10:50:01 +0800

    Update ImageInput Replace TouchableOpactiy with TouchableWithOutFeedback

--- app/components/ImageInput.tsx
+++ app/components/ImageInput.tsx
@@ -1,49 +1,97 @@
 import React, { useEffect } from "react";
-import { StyleSheet, View, ViewStyle } from "react-native";
+import {
+  StyleSheet,
+  View,
+  ViewStyle,
+  Image,
+  TouchableWithoutFeedback,
+  Alert,
+} from "react-native";
 import * as ImagePicker from "expo-image-picker";
-import { TouchableOpacity } from "react-native-gesture-handler";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { colors } from "../config/colors";
+import { ImageType } from "../config/types";
 
 interface Props {
   size?: number;
-  onPress: (...args: any) => any;
   style?: ViewStyle;
+  imageUri?: ImageType;
+  onChangeImage: (imageUri: ImageType) => any;
 }
 
-export const ImageInput = ({ style, size = 100, onPress }: Props) => {
+export const ImageInput = ({
+  imageUri,
+  style,
+  size = 100,
+  onChangeImage,
+}: Props) => {
   useEffect(() => {
-    const requestPermission = async () => {
-      try {
-        const { granted } =
-          await ImagePicker.requestMediaLibraryPermissionsAsync();
-        if (!granted) {
-          alert("Permission to access camera is required!");
-          return;
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
     requestPermission();
   }, []);
 
+  const requestPermission = async () => {
+    try {
+      const { granted } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (!granted) {
+        alert("Permission to access camera is required!");
+        return;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 0.5,
+      });
+      if (!result.cancelled) onChangeImage(result.uri);
+    } catch (error) {
+      console.log("Error reading image", error);
+    }
+  };
+
+  const handlePress = () => {
+    if (!imageUri) selectImage();
+    else
+      Alert.alert("Delete", "Are you sure you want to delete this image?", [
+        { text: "Yes", onPress: () => onChangeImage(null) },
+        { text: "No" },
+      ]);
+  };
+
   return (
-    <TouchableOpacity onPress={onPress}>
+    <TouchableWithoutFeedback onPress={handlePress}>
       <View style={[styles.container, style, { width: size, height: size }]}>
-        <MaterialCommunityIcons name="camera" size={size / 2} color="#3c3c3c" />
+        {imageUri ? (
+          <Image source={{ uri: imageUri }} style={styles.image} />
+        ) : (
+          <MaterialCommunityIcons
+            name="camera"
+            size={size / 2}
+            color={colors.darkGray}
+          />
+        )}
       </View>
-    </TouchableOpacity>
+    </TouchableWithoutFeedback>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    borderRadius: 20,
-    height: 100,
-    width: 100,
-    backgroundColor: colors.lightGray,
     alignItems: "center",
+    borderRadius: 20,
+    backgroundColor: colors.lightGray,
+    height: 100,
     justifyContent: "center",
+    width: 100,
+    overflow: "hidden",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
