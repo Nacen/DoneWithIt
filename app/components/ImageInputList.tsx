13fdc36e543c0fddd11d30f9d9354faf3fa4b782
NacenVi <overlordnacen@gmail.com>
2021-06-26 10:51:23 +0800

    Update ImageInput List use map instead of a flatlist

--- app/components/ImageInputList.tsx
+++ app/components/ImageInputList.tsx
@@ -1,50 +1,42 @@
-import React, { useState } from "react";
-import { StyleSheet, View, Image } from "react-native";
-import { FlatList } from "react-native-gesture-handler";
-import * as ImagePicker from "expo-image-picker";
+import React from "react";
+import { StyleSheet, View, ScrollView } from "react-native";
 
 import { ImageInput } from "./ImageInput";
-import { isObjectEmpty } from "../config/utils";
+import { ImageType } from "../config/types";
+import { useRef } from "react";
 
-interface Props {}
+interface Props {
+  images?: ImageType[];
+  onAddImage: (image: ImageType) => void;
+  onRemoveImage: (image: ImageType) => void;
+}
 
-type ImageType = {
-  uri: "string";
-};
-
-export const ImageInputList = ({}: Props) => {
-  const [images, setImages] = useState<ImageType[]>([]);
-
-  const setImageFromLibrary = async () => {
-    try {
-      let result = await ImagePicker.launchImageLibraryAsync();
-      if (result.cancelled) return;
-      console.log(result);
-      setImages((images) => [...images, { uri: (result as ImageType)["uri"] }]);
-      return result;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+export const ImageInputList = ({
+  images = [],
+  onAddImage,
+  onRemoveImage,
+}: Props) => {
+  const scrollView = useRef<ScrollView>(null);
 
   return (
-    <View style={styles.container}>
-      {!isObjectEmpty(images) && (
-        <FlatList
-          data={images}
-          renderItem={({ item }) => (
-            <Image source={{ uri: item.uri }} style={styles.image} />
-          )}
-          keyExtractor={(item, index) => index.toString()}
-          style={styles.imageList}
-          numColumns={3}
-        />
-      )}
-      <ImageInput
-        onPress={setImageFromLibrary}
-        size={75}
-        style={styles.imageInput}
-      />
+    <View>
+      <ScrollView
+        horizontal
+        ref={scrollView}
+        onContentSizeChange={() => scrollView.current?.scrollToEnd()}
+      >
+        <View style={styles.container}>
+          {images.map((image, index) => (
+            <View key={`${image}${index}`} style={styles.image}>
+              <ImageInput
+                imageUri={image}
+                onChangeImage={() => onRemoveImage(image)}
+              />
+            </View>
+          ))}
+          <ImageInput onChangeImage={(image) => onAddImage(image)} />
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -52,19 +44,8 @@ export const ImageInputList = ({}: Props) => {
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
-    flexWrap: "wrap",
   },
   image: {
-    width: 75,
-    height: 75,
     marginRight: 10,
-    marginBottom: 10,
-  },
-  imageInput: {
-    marginHorizontal: 10,
-  },
-  imageList: {
-    backgroundColor: "red",
-    flexShrink: 1,
   },
 });
