4be6aea67879fd9ff3f4f8481b06f1a845cb9899
Nacen <overlordnacen@gmail.com>
2021-07-01 12:23:07 +0800

    Create routes object

---
+++ app/navigation/routes.ts
@@ -0,0 +1,8 @@
+export const routes = Object.freeze({
+  LISTING_DETAILS: "ListingDetails",
+  LISTING_EDIT: "ListingEdit",
+  LOGIN: "Login",
+  MESSAGES: "Messages",
+  REGISTER: "Register",
+  ACCOUNT: "Account",
+});
