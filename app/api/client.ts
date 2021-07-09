c0620908f0f091f04a6510425e9e6d95c68f15ba
Nacen <overlordnacen@gmail.com>
2021-07-09 10:33:54 +0800

    Edit timeout for apisauce client

--- app/api/client.ts
+++ app/api/client.ts
@@ -1,6 +1,6 @@
 import { create } from "apisauce";
 
-const timeout = 5000; // 5 seconds
+const timeout = 10000; // 5 seconds
 
 export const apiClient = create({
   baseURL: "http://192.168.42.12:9000/api",
